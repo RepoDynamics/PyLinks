cd1abe6abe4fa4abdccd1bf3e5bd30610618d331
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-05-30 00:26:58 +0200

    update pypi module and add docstrings

--- pylinks/pypi.py
+++ pylinks/pypi.py
@@ -1,34 +1,62 @@
+"""URLs for PyPI packages."""
+
 
 from typing import Optional
-from pylinks import URL
-from pylinks import OFFLINE_MODE
 import re
+
 import requests
 
+import pylinks
+from pylinks import url
+from pylinks.url import URL
 
-BASE_URL = URL(base="https://pypi.org")
+
+BASE_URL = url("https://pypi.org")
 
 
-class Project:
+class Package:
+    """A PyPI Python package."""
 
     def __init__(self, name: str, validate: Optional[bool] = None):
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the package.
+        validate : bool, default: None
+            Whether to validate the URL online (requires an active internet connection).
+            If set to None (default), the global default value defined in `pylinks.OFFLINE_MODE` is used.
+        """
         if not isinstance(name, str):
             raise TypeError(f"`name` must be a string, not {type(name)}.")
         if re.match("^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$", name, flags=re.IGNORECASE) is None:
             raise ValueError('Distribution name is invalid; see https://peps.python.org/pep-0508/#names.')
         self._name = name
-        if validate is True or (validate is None and not OFFLINE_MODE):
-            requests.get(str(self.home)).raise_for_status()
-
-    @property
-    def home(self) -> URL:
-        return BASE_URL / 'project' / self.name
+        if validate is True or (validate is None and not pylinks.OFFLINE_MODE):
+            requests.get(str(self.homepage)).raise_for_status()
 
     @property
     def name(self) -> str:
+        """Name of the package."""
         return self._name
 
+    @property
+    def homepage(self) -> URL:
+        """URL of the package homepage."""
+        return BASE_URL / 'project' / self.name
 
-def project(name: str, validate: Optional[bool] = None) -> Project:
-    return Project(name=name, validate=validate)
+
+def package(name: str, validate: Optional[bool] = None) -> Package:
+    """
+    Create a new URL generator for a PyPI package.
+
+    Parameters
+    ----------
+    name : str
+       Name of the package.
+    validate : bool, default: None
+       Whether to validate the URL online (requires an active internet connection).
+       If set to None (default), the global default value defined in `pylinks.OFFLINE_MODE` is used.
+    """
+    return Package(name=name, validate=validate)
 
