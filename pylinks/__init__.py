8fd67ed50ecd7f21ffdf25655373d2e76a29ad6e
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-05-29 21:46:59 +0200

    update main __init__

--- pylinks/__init__.py
+++ pylinks/__init__.py
@@ -1,5 +1,15 @@
+"""PyLinks: Create, Modify and Manage URLs.
+
+PyLinks implements a URL object, making it easy to manipulate and work with URLs.
+It also offers a number of URL generators for popular online platforms
+such as GitHub, PyPI, Anaconda, ReadTheDocs etc., allowing for facile and dynamic
+creation of many useful URLs.
+"""
+
+
 from .url import URL
-from . import github
+from . import binder, conda, github, pypi, readthedocs
 
 
 OFFLINE_MODE: bool = False
+"""Global variable to set whether URL generators should verify the created URLs online."""
