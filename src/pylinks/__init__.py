c417c16129f47ee571a46a9cbc8ebfdba31b2253
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-08-07 05:29:56 +0200

    chore: update infrastructure

--- src/pylinks/__init__.py
+++ src/pylinks/__init__.py
@@ -11,11 +11,7 @@ but can be used directly from the root. It returns a URL object, also defined in
 Other available modules offer shortcuts for creating useful URLs for popular online services.
 """
 
-OFFLINE_MODE: bool = False
-"""Global variable to set whether URL generators should verify the created URLs online."""
-
-
+from ._settings import settings
 from .url import url
-from . import binder, conda, github, pypi, readthedocs
 from .http import request
-
+from . import api, site
--- src/pylinks/site/readthedocs.py
+++ src/pylinks/site/readthedocs.py
@@ -1,14 +1,16 @@
 """URLs for ReadTheDocs websites."""
 
-from typing import Optional
-import requests
 
-import pylinks
-from pylinks import url
+# Standard libraries
+from typing import Optional
+
+# Non-standard libraries
+import requests
+from pylinks import settings, url
 from pylinks.url import URL
 
 
-BASE_URL = url(url='https://readthedocs.org')
+BASE_URL = url(url="https://readthedocs.org")
 
 
 class Project:
@@ -27,7 +29,7 @@ class Project:
         if not isinstance(name, str):
             raise TypeError(f"`name` must be a string, not {type(name)}.")
         self._name = name
-        if validate is True or (validate is None and not pylinks.OFFLINE_MODE):
+        if validate is True or (validate is None and not settings.offline_mode):
             requests.get(str(self.project_home)).raise_for_status()
         return
 
@@ -39,21 +41,21 @@ class Project:
     @property
     def project_home(self) -> URL:
         """URL of the project's homepage. This is not the homepage of the website."""
-        return BASE_URL / 'projects' / self.name
+        return BASE_URL / "projects" / self.name
 
     @property
     def build_status(self) -> URL:
         """URL of the webpage showing an overview of the website's build status."""
-        return self.project_home / 'builds'
+        return self.project_home / "builds"
 
     @property
     def homepage(self) -> URL:
         """URL of the website's homepage."""
-        return url(f'https://{self.name}.readthedocs.io')
+        return url(f"https://{self.name}.readthedocs.io")
 
 
 def project(
-        name: str,
-        validate: Optional[bool] = None,
+    name: str,
+    validate: Optional[bool] = None,
 ):
     return Project(name=name, validate=validate)
